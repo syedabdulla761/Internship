2d704926508fc84f0732a3d3304db5eaf895a842
Syed Abdulla <syedabdulla761@gmail.com>
2023-03-02 17:37:19 +0530

    practice folder

---
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+
+namespace MyFirstApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ArrayList x = new ArrayList();
+            ArrayList y = new ArrayList(10);
+            x.Add(1);
+            y.Add(2);
+            x.Add("syed");
+            x.Add(1.5);
+            foreach (object item in x)
+            {
+                Console.WriteLine(item);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ MyFirstApp/MyFirstApp/trialClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp
+{
+    class trialClass
+    {
+        public int l { get; set; }
+        public int h { get; set; }
+        public int w { get; set; }
+        public int v { get; set; }
+
+        public void displayinfo()
+        {
+            Console.WriteLine("Length is {0}, height is {1}, width is {2}, volume is {3}",l,h,w,v=l*h*w);
+        }
+    }
+}
