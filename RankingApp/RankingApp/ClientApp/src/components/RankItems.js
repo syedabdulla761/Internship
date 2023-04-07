7e67dbfd039a4c43783682f3eb6ecaeaff65689b
Syed Abdulla <syedabdulla761@gmail.com>
2023-04-07 19:19:21 +0530

    Incomplete react api,still in progress

---
+++ RankingApp/RankingApp/ClientApp/src/components/RankItems.js
@@ -0,0 +1,33 @@
+﻿import React, { useState, useEffect } from 'react';
+import MovieImageArr from './MovieImages';
+
+const RankItems = () => {
+
+    const [items, setItems] = useState([]);
+    const dataType = 1;
+    useEffect(() => {
+        fetch(`item/${dataType}`)
+            .then((results) => {
+                return results.json();
+            })
+            .then(data => {
+                setItems(data);
+            })
+    },[])
+
+    return (
+
+        <main>
+        <div className="items-not-ranked">
+        { 
+                    (items != null) ? items.map((item) =>
+                    <div className="unranked-cell">
+                        <img id={`item-${item.id}`} src={MovieImageArr.find(o=>o.id===item.imageId)?.image }/>
+                    </div>
+                    ): <div>Loading....</div>
+         }
+        </div>
+        </main>
+    )
+}
+export default RankItems;
