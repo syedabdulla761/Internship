8806134bc08d16878cd29c40b7054136e3f87857
Syed Abdulla <syedabdulla761@gmail.com>
2023-04-11 00:27:08 +0530

    almost finished ,but now concentrating on logging

--- RankingApp/RankingApp/ClientApp/src/components/RankItems.js
+++ RankingApp/RankingApp/ClientApp/src/components/RankItems.js
@@ -1,13 +1,23 @@
 ﻿import React, { useState, useEffect } from 'react';
 import MovieImageArr from './MovieImages';
+import RankingGrid from './RankingGrid';
 
 const RankItems = () => {
 
     const [items, setItems] = useState([]);
     const dataType = 1;
+
+    function drag(ev) {
+        ev.dataTransfer.setdata("text", ev.target.id);
+    }
+
+    function allowDrop(ev) {
+        ev.preventDefault();
+    }
+
     useEffect(() => {
         fetch(`item/${dataType}`)
-            .then((results) => {
+            .then((results) => {  
                 return results.json();
             })
             .then(data => {
@@ -18,11 +28,12 @@ const RankItems = () => {
     return (
 
         <main>
+            <RankingGrid items={items} imgArr={MovieImageArr}/>
         <div className="items-not-ranked">
         { 
                     (items != null) ? items.map((item) =>
                     <div className="unranked-cell">
-                        <img id={`item-${item.id}`} src={MovieImageArr.find(o=>o.id===item.imageId)?.image }/>
+                            <img alt={item.title} id={`item-${item.id}`} src={MovieImageArr.find(o=>o.id===item.imageId)?.image }/>
                     </div>
                     ): <div>Loading....</div>
          }
