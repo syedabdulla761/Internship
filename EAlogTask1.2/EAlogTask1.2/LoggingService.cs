6a7be36c239452691115ca21906641b5015b4f2d
Syed Abdulla <syedabdulla761@gmail.com>
2023-04-19 10:56:04 +0530

    This is project is of logging using Everyangle.logging as a shared library this is the first step i.e twwo tasks executed by us two interns. 1.generating the csl logs, 2.Creating a dll file of everyangle.logging and using it locally rather than using it as a nuget package.

---
+++ EAlogTask1.2/EAlogTask1.2/LoggingService.cs
@@ -0,0 +1,13 @@
+﻿using EveryAngle.Logging;
+
+public static class LoggingService
+{
+    public static void Initialize()
+    {
+        var logFolder = "C:\\Users\\syed.abdulla\\source\\repos\\EAlogTask1.2\\Logs"; // Replace with your desired log folder path
+        var maxLogFileSize = 1024; // Replace with your desired max log file size in kilobytes
+        var maxLogFileNumber = 5; // Replace with your desired max number of log files
+
+        Log.Initialize(logFolder, maxLogFileSize, maxLogFileNumber, true, "MyConsoleApp");
+    }
+}
---
+++ EAlogTask1.2/EAlogTask1.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using EveryAngle.Logging;
+using System;
+
+namespace EALogTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialize the logger
+            LoggingService.Initialize();
+
+            // Log a message at different severity levels
+            Log.SendInfo("Info log message");
+            Log.SendWarning("Warning log message");
+            Log.SendError("Error log message");
+
+            // Log a message at the "info" severity level
+            Log.SendInfo("Hello, world!");
+
+            // Log a message at the "error" severity level, with an exception
+            try
+            {
+                int x = 0;
+                int y = 1 / x;
+            }
+            catch (Exception ex)
+            {
+                Log.SendError("An error occurred: " + ex.Message, ex);
+            }
+            Console.ReadLine();
+        }
+    }
+}
